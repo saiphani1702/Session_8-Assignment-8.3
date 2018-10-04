7ac68407f18a2b72e11ddb663abeb133d775a171
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-04 16:10:07 +0530

    Update assign8-3.js

--- assign8-3.js
+++ assign8-3.js
@@ -1,5 +1,6 @@
+//displaying multiplication table by using functions
 var table=function(n){
-for(var i=1;i<=10;i++)
-    console.log(`${n} * ${i} = ${n*i}`);
+for(var i=1;i<=10;i++)//for loop to verify the conditions
+    console.log(`${n} * ${i} = ${n*i}`);//displays output to the console
 }
-console.log(table(10));
+console.log(table(10));//displays output to the console by calling the function
